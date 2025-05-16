96c94bf27a095cefe5d52eb26921818c2a6a3b29
Marcus Sorensen <msorensen@nvidia.com>
2025-05-16 16:50:09 -0600

    feat: add function to render frames that are wide

--- lib/display.py
+++ lib/display.py
@@ -13,10 +13,14 @@ from PIL import Image, ImageFont, ImageDraw
 from lib.control import TargetMemory
 
 label_font = ImageFont.truetype("lib/font/LiberationMono-Regular.ttf", 16)
-value_font = ImageFont.truetype("lib/font/Quicksand-Regular.ttf", 36)
-value_font_bold = ImageFont.truetype("lib/font/Quicksand-Bold.ttf", 36)
+label_font_mid = ImageFont.truetype("lib/font/LiberationMono-Regular.ttf", 20)
+label_font_lg = ImageFont.truetype("lib/font/LiberationMono-Regular.ttf", 24)
+value_font = ImageFont.truetype("lib/font/Quicksand-Regular.ttf", 24)
+value_font_lg = ImageFont.truetype("lib/font/Quicksand-Regular.ttf", 36)
+value_font_lg_bold = ImageFont.truetype("lib/font/Quicksand-Bold.ttf", 36)
 
 bg_color = "BLACK"
+light_bg_color = "DIMGREY"
 fg_color = "WHITE"
 
 
@@ -77,8 +81,8 @@ class FlowGraph:
         fmt_flow_label = "g/s"
         w = draw.textlength(fmt_flow, value_font)
         wl = draw.textlength(fmt_flow_label, label_font)
-        draw.text(((236 - w - wl), 0), fmt_flow, fg_color, value_font)
-        draw.text(((240 - wl), value_font.size - label_font.size), fmt_flow_label, fg_color, label_font)
+        draw.text(((self.x_pix - 4 - w - wl), (self.y_pix * .25) - value_font.size - 4), fmt_flow, fg_color, value_font)
+        draw.text(((self.x_pix - wl), (self.y_pix * .25) - label_font.size - 4), fmt_flow_label, fg_color, label_font)
         return img
 
     def __draw_y_line(self, draw: ImageDraw, y, color):
@@ -197,8 +201,13 @@ class Display:
                 logging.error("Skipping display redraw because battery value is missing")
                 continue
 
-            img = draw_frame(self.lcd.width, self.lcd.height, data)
-            if data.save_image:
+            img = None
+            if self.lcd.width = 240:
+                img = draw_frame(self.lcd.width, self.lcd.height, data)
+            else:
+                img = draw_frame_wide(self.lcd.width, self.lcd.height, data)
+
+            if data.save_image and img is not None:
                 self.save_image(img)
             self.lcd.ShowImage(img, 0, 0)
 
@@ -223,23 +232,23 @@ def draw_frame(width: int, height: int, data: DisplayData) -> Image:
 
     # weight value
     fmt_weight = "{:0.1f}".format(data.weight)
-    w = draw.textlength(fmt_weight, value_font)
-    h = value_font.size
-    draw.text(((120 - w) / 2, (108 - h) / 2), fmt_weight, fg_color, value_font)
+    w = draw.textlength(fmt_weight, value_font_lg)
+    h = value_font_lg.size
+    draw.text(((120 - w) / 2, (108 - h) / 2), fmt_weight, fg_color, value_font_lg)
 
     # target value
     fmt_target = "{:0.1f}".format(data.memory.target)
-    target_font = value_font
+    target_font = value_font_lg
     w = draw.textlength(fmt_target, target_font)
     h = target_font.size
     draw.text(((120 - w) / 2 + 120, (108 - h) / 2), fmt_target, fg_color, target_font)
 
     fmt_ready = "Ready"
-    w = draw.textlength(fmt_ready, value_font)
-    h = value_font.size
+    w = draw.textlength(fmt_ready, value_font_lg)
+    h = value_font_lg.size
     h_pos = 164
     draw.rectangle((116 - w / 2, h_pos, 124 + w / 2, h_pos + h + 4), bg_color, data.memory.color, 4)
-    draw.text((120 - w / 2, h_pos), fmt_ready, fg_color, value_font)
+    draw.text((120 - w / 2, h_pos), fmt_ready, fg_color, value_font_lg)
 
     if data.flow_data is not None and len(data.flow_data) > 0:
         flow_rate_data = data.flow_rate_moving_avg()
@@ -254,5 +263,65 @@ def draw_frame(width: int, height: int, data: DisplayData) -> Image:
         draw.text(((240 - w) / 2, 262), fmt_shot_time, fg_color, label_font)
 
         img.paste(flow_image, (0, 98))
+
+    return img
+
+def draw_frame_wide(width: int, height: int, data: DisplayData) -> Image:
+    background = bg_color
+    if data.paddle_on:
+        background = light_bg_color
+    img = Image.new("RGBA", (width, height), background)
+
+    draw = ImageDraw.Draw(img)
+
+    # main boxes are 106 wide x 72 high
+    draw.line([(0, 72), (320, 72)], fill=fg_color, width=2)
+    draw.line([(106, 0), (106, 72)], fill=fg_color, width=2)
+    draw.line([(212, 0), (212, 72)], fill=fg_color, width=2)
+    draw.line([(0, 285), (240, 285)], fill=fg_color, width=2)
+
+    # weight and target labels
+    draw.text((10, 8), "weight(g)", fg_color, label_font)
+    draw.text((118, 8), "tgt %s (g)" % data.memory.name, fg_color, label_font)
+    draw.text((234, 8), "battery", fg_color, label_font)
+
+    # weight value
+    fmt_weight = "{:0.1f}".format(data.weight)
+    w = draw.textlength(fmt_weight, value_font_lg)
+    h = value_font_lg.size
+    draw.text(((106 - w) / 2, (88 - h) / 2), fmt_weight, fg_color, value_font_lg)
+
+    # target value
+    fmt_target = "{:0.1f}".format(data.memory.target)
+    target_font = value_font_lg
+    w = draw.textlength(fmt_target, target_font)
+    h = target_font.size
+    draw.text(((106 - w) / 2 + 106, (88 - h) / 2), fmt_target, fg_color, target_font)
+
+    # battery value
+    fmt_batt = "%d%%" % data.battery
+    w = draw.textlength(fmt_batt, value_font_lg)
+    draw.text(((106 - w)/2 + 214, (88 - h) / 2), fmt_batt, fg_color, value_font_lg)
+
+    fmt_ready = "Ready"
+    w = draw.textlength(fmt_ready, value_font_lg)
+    h = value_font_lg.size
+    h_pos = 120
+    draw.rectangle((156 - w / 2, h_pos, 164 + w / 2, h_pos + h + 4), bg_color, data.memory.color, 4)
+    draw.text((160 - w / 2, h_pos), fmt_ready, fg_color, value_font_lg)
+
+    if data.flow_data is not None and len(data.flow_data) > 0:
+        flow_rate_data = data.flow_rate_moving_avg()
+        flow_image = FlowGraph(flow_rate_data, data.memory.color, width_pixels=320, height_pixels=132).generate_graph()
+        last_sample_time = data.sample_rate * float(len(data.flow_data))
+
+        draw.text((4, 212), "%ds" % math.ceil(last_sample_time), fg_color, label_font)
+        draw.text((298, 212), "0s", fg_color, label_font)
+
+        fmt_shot_time = "timer:{:0.1f}s".format(data.shot_time_elapsed)
+        w = draw.textlength(fmt_shot_time, label_font_lg)
+        draw.text(((320 - w) / 2, 208), fmt_shot_time, fg_color, label_font_lg)
+
+        img.paste(flow_image, (0, 72))
 
     return img
